8e4411c759dfbe7964dff2a9f55cf1d89d371211
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-03-13 22:46:56 +0900

    delete no use file

--- app/controllers/home_controller.rb
+++
@@ -1,7 +0,0 @@
-class HomeController < ApplicationController
-  skip_before_action :require_login, only: [:index]
-
-  def index
-    @grumbles = Grumble.order(created_at: :desc).page(params[:page])
-  end
-end
