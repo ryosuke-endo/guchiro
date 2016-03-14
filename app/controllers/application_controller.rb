c3a16d9489fc17a68100a06de4724d44a2ada0d2
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-03-14 16:44:06 +0900

    delete no use helper and view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,17 @@ class ApplicationController < ActionController::Base
   before_filter :require_login
 
   private
-    def not_authenticated
-      redirect_to login_path, alert: 'Please login first'
-    end
+  def not_authenticated
+    redirect_to login_path, alert: 'Please login first'
+  end
 
-    def anonymous_name
-      user_ip = request.ip
-      day = Date.today.to_s
-      anonymous_name = user_ip + day
-    end
+  def anonymous_name
+    user_ip = request.ip
+    day = Date.today.to_s
+    anonymous_name = user_ip + day
+  end
 
-    def anonymous_digest(anonymous_name, character_length)
-      anonymous_digest = Digest::SHA512.hexdigest(anonymous_name).slice(0..character_length)
-    end
+  def anonymous_digest(anonymous_name, character_length)
+    anonymous_digest = Digest::SHA512.hexdigest(anonymous_name).slice(0..character_length)
+  end
 end
