3c12ab28d08db7ac11668c8c6c252ab28a3c6615
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-03-18 18:29:30 +0900

    fix mypage list link

--- spec/features/users_logins_spec.rb
+++ spec/features/users_logins_spec.rb
@@ -17,4 +17,11 @@ RSpec.feature "UsersLogins", type: :feature do
     visit edit_user_path(@other_user)
     expect(current_path).to eq user_path(@user)
   end
+
+  it 'check user post list page' do
+    login(@user)
+    visit user_path(@user)
+    find('.my_post-grumble').click
+    expect(current_path).to eq user_grumbles_path(@user)
+  end
 end
