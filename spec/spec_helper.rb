6c5774bf71a37cd6df0340646e7ae9836ca1352e
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-04-09 13:34:12 +0900

    add test coverage performance gem

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -16,6 +16,12 @@
 # users commonly want.
 #
 # See http://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
+require 'simplecov'
+# coverage performance tool configuration
+SimpleCov.start do
+  add_filter '/vendor/'
+end
+
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
