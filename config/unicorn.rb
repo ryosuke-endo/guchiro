545416ef408f780691b49bce1df66f7d71509bc8
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-04-17 16:04:37 +0900

    change unicorn sock

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -10,7 +10,7 @@ worker_processes Integer(ENV['WEB_CONCURRENCY'])
 timeout 30
 preload_app true
 
-listen '/tmp/unicorn.spui.sock', backlog: 64
+listen '/tmp/unicorn.guchiro.sock', backlog: 64
 
 before_fork do |server, worker|
   Signal.trap 'TERM' do
