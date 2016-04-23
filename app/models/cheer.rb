a3c06a9774cab7a55a6db91ba6d05160b2e8c555
ryosuke-endo <frozen_faithjp2@yahoo.co.jp>
2016-04-23 16:05:59 +0900

    使っていないメソッドを削除

--- app/models/cheer.rb
+++ app/models/cheer.rb
@@ -1,10 +1,4 @@
 class Cheer < ActiveRecord::Base
   belongs_to :user
   belongs_to :grumble, counter_cache: true
-
-  class << self
-    def cheered_by_user?(user, grumble)
-      Cheer.where(user_id: user.id, grumble_id: grumble.id).exists?
-    end
-  end
 end
--- app/models/sympathy.rb
+++ app/models/sympathy.rb
@@ -1,10 +1,4 @@
 class Sympathy < ActiveRecord::Base
   belongs_to :user
   belongs_to :grumble, counter_cache: true
-
-  class << self
-    def sympathied_by_user?(user, grumble)
-      Sympathy.where(user_id: user.id, grumble_id: grumble.id).exists?
-    end
-  end
 end
